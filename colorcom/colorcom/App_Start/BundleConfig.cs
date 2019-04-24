ca547f4baa3da5cee9f101832a2042b3aeb8e46d
Marcio Xavier <juniorxavier606@gmail.com>
2019-04-23 23:04:18 -0300

    Ajustes nas importações

--- colorcom/colorcom/App_Start/BundleConfig.cs
+++ colorcom/colorcom/App_Start/BundleConfig.cs
@@ -8,22 +8,36 @@ namespace colorcom
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-{version}.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/Chart.min.js",
+                      "~/Scripts/popper.min.js",
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/assets").Include(
+                      "~/Scripts/dashboard.js",
+                      "~/Scripts/main.js",
+                      "~/Scripts/widget.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/chosen.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/style.css"));
+
+
+            bundles.Add(new StyleBundle("~/Content/themify-icons").Include(
+                      "~/Content/themify-icons/css/themify-icons.css"));
         }
     }
 }
